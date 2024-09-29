8c6468f2f51da3ddf1f35aeefdf8db068d9acaf0
sorongosdev <dosora9@gmail.com>
2024-09-29 18:12:14 +0900

    텍스트에 style추가

--- App.tsx
+++ App.tsx
@@ -5,16 +5,25 @@
 
 
 import React, {Component} from 'react';
-import {View,Text} from 'react-native';
+import {View,Text, StyleSheet} from 'react-native';
 
 class App extends Component{
   render() {
     return(
-      <View>
+      <View style={styles.background}>
         <Text>Hello World</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  background:{
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default App;
